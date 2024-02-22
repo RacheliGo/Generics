44f96e718859441139fdbb1c5cbf77033549d962
RacheliGo <racheli5910@gmail.com>
2024-02-22 23:42:24 +0200

    Add project files.

---
+++ Generics/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            double y = 5.5;
+            string z = "r";
+            Print<int>(x, "x");
+            Print<double>(y, "y");
+            Print<string>(z, "z");
+            
+
+            SwapList<int> listNum = new SwapList<int>() { 1, 2, 3 };
+            Console.WriteLine(listNum[0]);
+            listNum.Swap(0,2);
+            Console.WriteLine(listNum[0]);
+
+            SwapList<string> listString = new SwapList<string>() { "Racheli", "Rivki", "Ruti" };
+            Console.WriteLine(listString[0]);
+            listString.Swap(0, 2);
+            Console.WriteLine(listString[0]);
+            Console.Read();
+        }
+        static void Print<T>(T obj, string nameObj)
+        {
+            Console.WriteLine(nameObj +" = "+obj);
+        }
+    }
+}
---
+++ Generics/SwapList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class SwapList<T>: List<T>
+    {
+        public void Swap(int index1, int index2)
+        {
+            T temp = this[index1];
+            this[index1] = this[index2];
+            this[index2] = temp;
+        }
+    }
+}
